f1148e930b4c155b12f86e6c2565bc45ff456ae1
Voland91 <you@example.com>
2021-03-06 15:21:39 +0100

    fixed time, added some comments for my logic, created new const for more readability

--- src/components/Event/Event.tsx
+++ src/components/Event/Event.tsx
@@ -12,19 +12,23 @@ export interface EventProps {
 }
 
 const Event: FC<EventProps> = ({ title, description, background, eventDate }) => {
-    const year = eventDate.getFullYear();
-    const month = eventDate.getMonth() + 1;
-    const day = eventDate.getDate();
-    console.log(eventDate);
+    const year = eventDate.getUTCFullYear();
+    //getMonth - returns month 0-11 so we need +1 to get right number
+    const month = eventDate.getUTCMonth() + 1;
+    const day = eventDate.getUTCDate();
+    const time = eventDate.toTimeString().slice(0, 8);
+
+    // adding 0 when number of a month or a day is less than 10, ex. 03.04 for march 4th
+    const fullDate = ` / ${month < 10 ? `0${month}` : month} / ${day < 10 ? `0${day}` : day}`;
 
     return (
         <EventWrapper background={background}>
             <DateWrapper>
                 <div>
                     <ColoredYear>{year}</ColoredYear>
-                    {` / ${month < 10 ? `0${month}` : month} / ${day < 10 ? `0${day}` : day}`}
+                    {fullDate}
                 </div>
-                <div>20:41:06 GMT</div>
+                <div>{`${time} UTC`}</div>
             </DateWrapper>
             <DescriptionWrapper>
                 <Title title={title} />
