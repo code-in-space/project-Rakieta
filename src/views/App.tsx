9ab269bf34230864a9d7f88e4ef88b874167ccdd
Rafal <rafal.szczuka05@gmail.com>
2021-03-06 17:39:32 +0100

    build test

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -12,7 +12,7 @@ const App: FC = () => (
         <Event
           eventDate={new Date('2021-03-01T12:05:00Z')}
           description="NASA TV will livestream the rendezvous and capture of Northrop Grumman's NG-15 Cygnus cargo craft to the International Space Station."
-          title="New Arrive"
+          title="New Arrive BUILD TEST"
         />
       </div>
     </ThemeProvider>
