29086dda9da9bee94092c75ade768ae64762b2b7
Rafal <rafal.szczuka05@gmail.com>
2021-03-06 21:38:29 +0100

    created basic tests

--- src/components/Description/Description.styles.tsx
+++ src/components/Description/Description.styles.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const StyledDescription = styled.div`
   font-size: ${({ theme }) => theme.fontSizes.m};
-  font-weight: ${({ theme }) => theme.fontWeights.normal};
+  font-weight: ${({ theme }) => theme.fontWeights.thin};
 `;
 
 export default StyledDescription;
