b1457c2f7fd63f89ada26c49cd603e7b620fc1af
Rafal <rafal.szczuka05@gmail.com>
2021-03-10 12:59:20 +0100

    added logo link to home page

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -2,11 +2,14 @@ import { FC } from 'react';
 import { HeaderWrapper } from './Header.styles';
 import Menu from './Menu/Menu';
 import { ReactComponent as Logo } from '../../../assets/icons/logo.svg';
+import { Link } from 'react-router-dom';
 
 export const Header: FC = () => {
   return (
     <HeaderWrapper>
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
       <Menu />
     </HeaderWrapper>
   );
