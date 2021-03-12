2c8218b7130563467b78016cea9bfe1fe14d8d4b
Mateusz Butkiewicz <butkiewicz.mat@gmail.com>
2021-03-12 10:32:15 +0100

    added stories for link component

---
+++ src/components/atoms/Link/Link.stories.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { Meta } from '@storybook/react';
+import GlobalStyle from '../../../theme/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../../theme/mainTheme';
+import { StyledLink } from '../../atoms/Link/Link.styles';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Description from '../Description/Description';
+
+export default {
+  title: 'Links',
+  component: StyledLink,
+} as Meta;
+
+export const Links: FC = () => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Route>
+          <StyledLink to="#">
+            <Description description="Sample link without styles" hero />
+          </StyledLink>
+        </Route>
+      </Router>
+    </ThemeProvider>
+  </>
+);
