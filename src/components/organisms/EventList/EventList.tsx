2fa00cb905fdfb3e95877f85a92bc43bf5ac0848
Voland91 <you@example.com>
2021-03-16 08:09:38 +0100

    replaced photos at bg, added useEffect in event list

--- src/components/organisms/EventList/EventList.tsx
+++ src/components/organisms/EventList/EventList.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useState, useEffect } from 'react';
 import useRequest, { RequestStatus } from '../../../hooks/useRequest';
 import EventItem from '../../molecules/EventItem/EventItem';
 import StyledEventListWrapper from './EventList.styles';
@@ -20,31 +20,30 @@ interface Event {
 
 const EventList: FC = () => {
   const [allEvents, setAllEvents] = useState<Event[] | null | undefined>(null);
-
   const events = useRequest<FetchedData>(`${API_BASE_URL}event/upcoming/`);
   const launches = useRequest<FetchedData>(`${API_BASE_URL}launch/upcoming/`);
+  useEffect(() => {
+    if (events.status === RequestStatus.FETCHED && launches.status === RequestStatus.FETCHED) {
+      const allData = events.data?.results;
+      launches.data?.results.map((launch) => {
+        // launches from API do not have key "date" but "net" so we create key "date" based on "net" to unify all data
+        launch.date = launch['net'];
 
-  const createEventsList = async () => {
-    const allData = events.data?.results;
-    await launches.data?.results.map((launch) => {
-      // launches from API do not have key "date" but "net" so we create key "date" based on "net" to unify all data
-      launch.date = launch['net'];
+        // launches also do not have "descrption" so we create key "description" to unify all data
+        launch.description = 'Launching Event 🚀';
 
-      // launches also do not have "descrption" so we create key "description" to unify all data
-      launch.description = 'Launching Event 🚀';
-
-      // avoiding repetition in data
-      allData?.indexOf(launch) === -1 && allData?.push(launch);
-    });
-    await setAllEvents(allData);
-  };
-
-  createEventsList();
-  console.log(allEvents);
+        // avoiding repetition in data
+        allData?.indexOf(launch) === -1 && allData?.push(launch);
+      });
+      setAllEvents(allData);
+    }
+  }, [events.status, launches.status]);
 
   return (
     <StyledEventListWrapper>
-      {allEvents === null && <Loader type="RevolvingDot" color={theme.colors.rose} height={100} width={100} />}
+      {(events.status === RequestStatus.FETCHING || launches.status === RequestStatus.FETCHING) && (
+        <Loader type="RevolvingDot" color={theme.colors.rose} height={100} width={100} />
+      )}
       {allEvents?.map((event) => (
         <EventItem eventDate={new Date(event.date)} description={event.description} title={event.name} key={event.id} />
       ))}
